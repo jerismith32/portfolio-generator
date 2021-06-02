5588271ec3fe4fefe857702d4309befe9db0bd4c
jerismith32 <jerismith32@gmail.com>
2021-06-02 17:00:25 -0400

    completed lesson 2

--- app.js
+++ app.js
@@ -1,18 +1,56 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => {
-      console.log(profileItem)
-    });
-  };
+const profileDataArgs = process.argv.slice(2);
 
-printProfileData(profileDataArgs);
+const [name, github] = profileDataArgs;
+
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+// // const name = profileDataArgs[0];
+// // const github = profileDataArgs[1];
+
+// const [firstName, github] = profileDataArgs;
+// console.log(firstName, github);
+// console.log(generatePage(firstName, github));
+
+// // const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+// // console.log(generatePage('Jane', 'janehub'));
+
+// const generatePage = (userName, githubName) => {
+//     return `
+//       Name: ${userName}
+//       GitHub: ${githubName}
+//     `;
+// };
+
+// console.log(generatePage(userName, github));
+
+
+//LESSON 1 COMPLETED CODE
+// console.log(profileDataArgs);
+
+// const printProfileData = profileDataArr => {
+//     // This...
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//       console.log(profileDataArr[i]);
+//     }
+  
+//     console.log('================');
+  
+//     // Is the same as this...
+//     profileDataArr.forEach((profileItem) => {
+//       console.log(profileItem)
+//     });
+//   };
+
+// printProfileData(profileDataArgs);
